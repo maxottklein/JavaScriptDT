5c2b99d3aa177a51c66e491e1237667ad9eedf3d
Max Klein <info@max-milian.com>
2019-02-19 19:26:11 -0500

    Update AssignmentTwo.js

--- AssignmentTwo.js
+++ AssignmentTwo.js
@@ -1,3 +1,4 @@
+  // 
 
   const chatbot ={
 
@@ -5,23 +6,55 @@
   home: 'I live in the clouds you upload your files to',
   age: 'I am 304856859433940 seconds old',
   food: 'I eat your data',
-  look: 'People say I\'m beautiful'
+  appearance: 'People say I\'m beautiful - as beautiful as a system could be...'
 
 };
+  
+
+
+  function answerQuestion(question, chatbot){
+
+
 
 
   if (question.toLowerCase().includes('name') || question.toLowerCase().includes('called') || question.toLowerCase().includes('who')){
-    console.log(bot.name);
-  } else if (question.toLowerCase().includes('home') || question.toLowerCase().includes('from')){
-    console.log(bot.home);
-  } else if (question.toLowerCase().includes('made') || question.toLowerCase().includes('built of')){
-    console.log(bot.madeOf);
-  } else if (question.toLowerCase().includes('food') || question.toLowerCase().includes('you eat') || question.toLowerCase().includes('eat')){
-    console.log(bot.food);
-  } else if (question.toLowerCase().includes('age') || question.toLowerCase().includes('old') || question.toLowerCase().includes('been around')){
-    console.log(bot.age);
+    console.log(chatbot.name);
+  } else if (question.toLowerCase().includes('home') || question.toLowerCase().includes('from') || question.toLowerCase().includes('where')){
+    console.log(chatbot.home);
+  } else if (question.toLowerCase().includes('age') || question.toLowerCase().includes('old') || question.toLowerCase().includes('how')){
+    console.log(chatbot.age);
+  } else if (question.toLowerCase().includes('food') || question.toLowerCase().includes('you eat') || question.toLowerCase().includes('eat') || question.toLowerCase().includes('what')){
+    console.log(chatbot.food);
+  } else if (question.toLowerCase().includes('look') || question.toLowerCase().includes('looking') || question.toLowerCase().includes('appearance')){
+    console.log(chatbot.appearance);
   }
   else {
-    console.log("It seem as I don't understand what you are trying to tell me");
+    console.log("It seems as I don't understand what you are trying to tell me :/");
   }
+}
+
+// alternatively 
+
+
+switch (chatbot) {
+  case (question.toLowerCase().includes('name') || question.toLowerCase().includes('called') || question.toLowerCase().includes('who')):
+    console.log(chatbot.name);
+    break;
+  case (question.toLowerCase().includes('home') || question.toLowerCase().includes('from') || question.toLowerCase().includes('where')): 
+    console.log(chatbot.home);
+    break;
+  case (question.toLowerCase().includes('age') || question.toLowerCase().includes('old') || question.toLowerCase().includes('how')):
+    console.log(chatbot.age);
+    break;
+  case (question.toLowerCase().includes('food') || question.toLowerCase().includes('you eat') || question.toLowerCase().includes('eat') || question.toLowerCase().includes('what')):
+    console.log(chatbot.food);
+    break;
+  case (question.toLowerCase().includes('age') || question.toLowerCase().includes('old') || question.toLowerCase().includes('been around')):
+    console.log(chatbot.appearance);
+    break;
+  default:
+    console.log("It seems as I don't understand what you are trying to tell me :/");
+    break;
+
+}
 
